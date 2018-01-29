92d6c508b180afb22f2849632d928017198db634
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2018-01-29 09:11:29 +0100

    make sure to copy the runall script, add an echo with explanation

--- experiments-maat/install.sh
+++ experiments-maat/install.sh
@@ -7,12 +7,16 @@ REMOTE="bwunicluster.scc.kit.edu:maat/"
 
 OLD="`pwd`"
 
-
 cd ${MAATDIR}
 gradle jar
-scp bin/libs/Maat-1.0.jar "$REMOTE"Maat.jar
-scp detectors.xml "$REMOTE"detectors.xml
-scp job.moab "$REMOTE"job.moab
-scp jobscript.sh "$REMOTE"jobscript.sh
+scp bin/libs/Maat-1.0.jar "${REMOTE}"Maat.jar
+scp testdata/test.json "${REMOTE}"test.json
+scp detectors.xml "${REMOTE}"detectors.xml
+scp job.moab "${REMOTE}"job.moab
+scp jobscript.sh "${REMOTE}"jobscript.sh
 
 cd ${OLD}
+
+scp runall.sh "${REMOTE}/../maat-runall.sh"
+
+echo "Install complete, please SSH to ${REMOTE} and execute ./maat-runall.sh to execute experiments"
