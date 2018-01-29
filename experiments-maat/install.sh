6d519cea1419cbc1b6547e4ba30a3185789d9c37
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2018-01-29 08:59:46 +0100

    fix error in install script

--- experiments-maat/install.sh
+++ experiments-maat/install.sh
@@ -2,7 +2,7 @@
 
 #run this on the machine where maat is stored in the dir specified below
 
-MAATDIR="~/research/maat"
+MAATDIR="${HOME}/research/maat"
 REMOTE="bwunicluster.scc.kit.edu:maat/"
 
 OLD="`pwd`"
@@ -14,3 +14,5 @@ scp bin/libs/Maat-1.0.jar "$REMOTE"Maat.jar
 scp detectors.xml "$REMOTE"detectors.xml
 scp job.moab "$REMOTE"job.moab
 scp jobscript.sh "$REMOTE"jobscript.sh
+
+cd ${OLD}
