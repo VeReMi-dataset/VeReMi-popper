cd8e0156e0e82041308687adfa0822ee0d7f36e2
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2018-01-29 08:48:10 +0100

    small script that checks whether veins-maat will execute correctly

---
+++ experiments-veins-maat/pre-validate.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+# test OMNET
+if ! ${HOME}/omnetpp/omnetpp-5.1.1/bin/opp_run -v;
+then
+  echo 'opp_run -v failed'
+fi
+
+# test SUMO
+if ! ${HOME}/sumo/sumo-0.30.0/bin/sumo --version;
+then
+  echo 'SUMO failed'
+elif [ -n "`${HOME}/sumo/sumo-0.30.0/bin/sumo --version | grep 'TRACI PROJ GDAL'`" ];
+  echo 'Your SUMO version lacks one or more of: TRACI, PROJ, GDAL'
+fi
+
+# test VEINS
+cd veins-maat/simulations/securecomm2018
+if ! ./validate.sh;
+then
+  echo 'validation script in your project failed, please look at the JSON output and remove it aftwards'
+else
+  echo 'removing validation output...'
+  find . -name '*.json' | xargs rm
+  rm -rf results
+  rm dua.static.log
+  rm dua.static.summary.xml
+  rm dua.static.tripinfo.xml
+  rm e1_OffRamp_output.xml
+  rm e1_OnRamp_output.xml
+  rm e1_begin_output.xml
+  rm e1_end_output.xml
+  rm e1_link_output.xml
+  rm .cmdenv-log
+fi
+
+cd ~
