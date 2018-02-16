e396d6f91b43e3ea36a5269b30d2e073ede26eba
Thomas Lukaseder <thomas.lukaseder@uni-ulm.de>
2018-02-16 14:08:25 +0100

    improved graph readability

--- dataset/boxplots.py
+++ dataset/boxplots.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib import rcParams
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Latin Modern Roman']
+plt.rcParams.update({'font.size': 14})
 
 high_csv = sys.argv[1]
 med_csv = sys.argv[2]
@@ -18,7 +19,7 @@ speed = sys.argv[4]
 
 if __name__ == "__main__":
   colors = [["#7D9AAA","#A9A280","#56AA1C","#A32638","#26247C","#BD6005"],["#b1c2ca","#c9c6b9","#96ca74","#c77a84","#7a98b0","#d59c69"]]
-  font = {'fontname':'Latin Modern Roman'}
+  font = {'fontname':'Latin Modern Roman','size':'14'}
 
   csvs = []
   with open(high_csv) as high, open(med_csv) as med, open(low_csv) as low:
@@ -51,6 +52,7 @@ if __name__ == "__main__":
 
 
     plt.figure()
+    #plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     for j in range(0,3):
       
       ax = plt.gca()    
@@ -70,7 +72,7 @@ if __name__ == "__main__":
     plt.xlabel(x_axis_label,**font)
 
     plt.legend()
-    plt.savefig(filename_hist,dpi=900)
+    plt.savefig(filename_hist,dpi=300)
 
     plt.figure()
     
@@ -81,4 +83,4 @@ if __name__ == "__main__":
     #plt.hist(data[j],bins)
     xi = [i for i in range(1, len(data)+1)]
     plt.xticks(xi, [high_csv.split('.')[0],med_csv.split('.')[0],low_csv.split('.')[0]])
-    plt.savefig(filename_box,dpi=900)
+    plt.savefig(filename_box,dpi=300)
